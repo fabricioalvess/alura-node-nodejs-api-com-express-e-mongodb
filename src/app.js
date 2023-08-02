ed58ce083190c9d073d9f7a5b05f02700751e0e9
Fabricio Alves <123993244+fabricioalvess@users.noreply.github.com>
2023-08-02 11:34:28 -0300

    Servicos utilizando os verbos http - pegar, pegar por id, adicionar, e atualizar.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"; 
-
 const app = express();
+app.use(express.json())
+
 
 const livros = [
     {
@@ -13,12 +14,32 @@ const livros = [
     }
 ]
 
+
 app.get('/', (req,res)=>{
     res.status(200).send('Curso de Node');
 })
-
 app.get('/livros', (req, res)=>{
     res.status(200).json(livros)
 })
+app.get('/livros/:id',(req, res)=>{
+    let index = buscarElementoPorId(req.params.id)
+    res.json(livros[index])
+})
+app.post('/livros',(req,res)=>{
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado com sucesso!')
+})
+app.put('/livros/:id',(req, res)=>{
+    let index = buscarElementoPorId(req.params.id)
+    livros[index].titulo = req.body.titulo
+    livros[index].id = req.body.id
+    res.json(livros)
+})
+
+
+function buscarElementoPorId(id){
+    return livros.findIndex(livro => livro.id == id) 
+}
+
 
 export default app
